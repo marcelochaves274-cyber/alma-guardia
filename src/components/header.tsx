c2b7cedd85ae62f47c1ca506c6f7e2dcf4d229b3
Marcelo Chaves Vaz <marcelochaves274@gmail.com>
2025-12-03 14:26:22 +0000

    agora eu quero na area de trabalho no titulo no canto esquerdo onde tem

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { SgsGeniusLogo } from '@/components/icons';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { useAppSettings } from '@/context/app-settings-context';
 import { Skeleton } from './ui/skeleton';
@@ -8,11 +7,11 @@ export function Header() {
   const { appName, isLoading } = useAppSettings();
   return (
     <header className="flex h-16 shrink-0 items-center border-b bg-background px-4 md:px-6">
-      <div className="flex w-1/3 justify-start">
-        <SidebarTrigger className="md:hidden" />
+      <div className="flex items-center gap-2">
+        <SidebarTrigger />
+        <h1 className="font-headline text-lg font-semibold hidden sm:block">Sistema de Gestão de Segurança</h1>
       </div>
       <div className="flex flex-1 flex-col items-center justify-center text-center">
-          <h1 className="font-headline text-lg font-semibold">Sistema de Gestão de Segurança</h1>
           {isLoading ? (
              <Skeleton className="h-4 w-32 mt-1" />
           ) : (
