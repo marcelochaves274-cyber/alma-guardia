2f052c2dbeeddf9f43c9271df79791c5282d5644
Marcelo Chaves Vaz <marcelochaves274@gmail.com>
2025-12-05 18:03:11 +0000

    vc nao consegue nem te ajudando, vc colocou uma barra de rolagem e o esp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
           </div>
         </header>
 
-        <main className="flex-1 overflow-y-auto p-4 md:p-6">
+        <main className="flex-1 overflow-y-hidden p-4 md:p-6">
           {renderContent()}
         </main>
       </SidebarInset>
