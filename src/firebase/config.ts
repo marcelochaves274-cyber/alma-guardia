b089352c8df2e9afcaa09b505154c7f1506eac74
Marcelo Chaves Vaz <marcelochaves274@gmail.com>
2025-12-07 15:03:30 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-// Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyARUFMEZXmZRD2GJEbQb4BrthjiVHc-mSA",
-  authDomain: "brave-drive-472322-m2.firebaseapp.com",
-  projectId: "brave-drive-472322-m2",
-  storageBucket: "brave-drive-472322-m2.appspot.com",
-  messagingSenderId: "768402541625",
-  appId: "1:768402541625:web:cf3ae4a28647d60c9491e8"
+  "projectId": "brave-drive-472322-m2",
+  "appId": "1:768402541625:web:cf3ae4a28647d60c9491e8",
+  "apiKey": "AIzaSyARUFMEZXmZRD2GJEbQb4BrthjiVHc-mSA",
+  "authDomain": "brave-drive-472322-m2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "768402541625"
 };
