365e727aeb7f404ee1c62f38ca0c2a35be6c62e6
Marcelo Chaves Vaz <marcelochaves274@gmail.com>
2025-12-07 14:35:53 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyBe_TJCgQ-i1KHBtYkG3jYt_FpFvO9F-EM",
+  apiKey: "AIzaSyARUFMEZXmZRD2GJEbQb4BrthjiVHc-mSA",
   authDomain: "brave-drive-472322-m2.firebaseapp.com",
   projectId: "brave-drive-472322-m2",
   storageBucket: "brave-drive-472322-m2.appspot.com",
-  messagingSenderId: "542287796074",
-  appId: "1:542287796074:web:0b297b0a7c6534575bfca4"
+  messagingSenderId: "768402541625",
+  appId: "1:768402541625:web:cf3ae4a28647d60c9491e8"
 };
