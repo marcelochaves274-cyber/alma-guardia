4dfc9914d7bc16b35798a759b17bfc1d73530410
Marcelo Chaves Vaz <marcelochaves274@gmail.com>
2025-12-13 16:53:35 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- src/components/risk-assessment-report.tsx
+++ src/components/risk-assessment-report.tsx
@@ -197,20 +197,22 @@ export function RiskAssessmentReport({ onEdit }: RiskAssessmentReportProps) {
     onEdit(assessment);
   };
   
-  const renderSkeletons = () => (
-    <>
-      {Array.from({ length: 5 }).map((_, i) => (
-        <TableRow key={i}>
-          <TableCell><Skeleton className="h-5 w-32" /></TableCell>
-          <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-          <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-          <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-          <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-          <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-        </TableRow>
-      ))}
-    </>
-  );
+  const renderSkeletons = () => {
+    return (
+      <>
+        {Array.from({ length: 5 }).map((_, i) => (
+          <TableRow key={i}>
+            <TableCell><Skeleton className="h-5 w-32" /></TableCell>
+            <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+            <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+            <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+            <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+            <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+          </TableRow>
+        ))}
+      </>
+    );
+  };
 
   return (
     <div className="space-y-6">
@@ -403,3 +405,4 @@ export function RiskAssessmentReport({ onEdit }: RiskAssessmentReportProps) {
     </div>
   );
 }
+    
