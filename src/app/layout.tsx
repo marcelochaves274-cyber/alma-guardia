db8762f516ad846b78aafc87605dadd6a1a2d9a9
Marcelo Chaves Vaz <marcelochaves274@gmail.com>
2026-01-23 14:00:16 +0000

    https://firebasestorage.googleapis.com/v0/b/brave-drive-472322-m2.fireba

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { FirebaseClientProvider } from '@/firebase/client-provider';
 import { HelpProvider } from '@/context/help-context';
 import { ProfileProvider } from '@/context/profile-context';
 
-const iconUrl = "https://firebasestorage.googleapis.com/v0/b/brave-drive-472322-m2.firebasestorage.app/o/Logo-da-SGS-APP-com-natureza%20icone.ico?alt=media&token=812f8f9d-5605-49cc-9b2f-35dfa026e41b";
+const iconUrl = "https://firebasestorage.googleapis.com/v0/b/brave-drive-472322-m2.firebasestorage.app/o/Logo-da-SGS-APP-ico.ico?alt=media&token=14f5c68a-515e-4c63-8774-d2ff889ba92a";
 
 export const metadata: Metadata = {
   title: 'SGS APP',
